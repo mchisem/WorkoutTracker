5fed688b30290cd033ba6965492514c599bf4e40
Maya Chisem <chisem.maya@gmail.com>
2020-10-27 21:29:56 -0700

    amending files and linking html's

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const express = require('express')
 
 // compressed compression function
 var app=express();function shouldCompress(s,e){return!s.headers["x-no-compression"]&&compression.filter(s,e)}app.use(compression({filter:shouldCompress}));
+
+
+const PORT = process.env.PORT || 8090;
+
+app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
